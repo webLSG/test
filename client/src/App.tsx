b25042d7ecb3b91c453350ebf7c4338a243fb111
LSG <sukyeng97@naver.com>
2024-08-09 21:23:14 +0900

    test3

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const App: React.FC = () => {
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    axios
+      .get("/api/hello")
+      .then((response) => {
+        setMessage(response.data.message);
+      })
+      .catch((error) => {
+        console.error("There was an error !", error);
+      });
+  }, []);
+
+  return <h1>{message}</h1>;
+};
+
+export default App;
